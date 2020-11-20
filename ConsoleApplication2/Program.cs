b6cc01ce824b30b70f28215ee9391c842a9ec886
Shervin <you@example.com>
2020-11-19 19:17:41 -0800

    Commited

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seasons;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter Season Name :");
+            seasons = Console.ReadLine();
+            switch (seasons)
+            {
+                case "spring":
+                    Console.WriteLine("January , February , March");
+                    break;
+                case "summer":
+                    Console.WriteLine("April , May , June");
+                    break;
+                case "fall":
+                    Console.WriteLine("July , August , September");
+                    break;
+                case "winter":
+                    Console.WriteLine("October , November , December");
+                    break;
+            }
+        }
+    }
+}
